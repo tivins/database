3de0a7f80cde256107031848ed20f09699d2b377
Tivins <tivins@elabo.fr>
2021-12-16 00:24:02 +0100

    Update functions.php

--- src/functions.php
+++ src/functions.php
@@ -3,15 +3,19 @@
 /**
  * Converts any items of `$array` as array.
  */
-function array_arrays(array $array) : array
-{
-    return array_map(fn($v) => is_array($v) ? $v : [$v], $array);
+if (!function_exists('array_arrays')) {
+    function array_arrays(array $array): array
+    {
+        return array_map(fn($v) => is_array($v) ? $v : [$v], $array);
+    }
 }
 
 /**
  * NB: All items should be arrays.
  */
-function array_flatten(array $array) : array
-{
-    return array_merge(...array_values($array));
+if (!function_exists('array_flatten')) {
+    function array_flatten(array $array): array
+    {
+        return array_merge(...array_values($array));
+    }
 }
