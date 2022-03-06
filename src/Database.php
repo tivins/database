a9f1f8df4bbc7386e6b34612b0269adbda82b9b9
Tivins <pub.git@elabo.fr>
2022-03-06 13:36:03 +0100

    Add sqliteCreateFunction.

--- src/Database.php
+++ src/Database.php
@@ -281,4 +281,9 @@ class Database
     {
         $this->handler->commit();
     }
+
+    public function sqliteCreateFunction($function_name, $callback, $num_args = -1, $flags = 0): bool
+    {
+        return $this->handler->sqliteCreateFunction($function_name, $callback, $num_args, $flags);
+    }
 }
